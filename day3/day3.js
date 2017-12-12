a7cb3becc20dc277b4a7deeef0bfc918aad4ebd9
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-12-13 00:02:56 +0100

    WIP Day 3

---
+++ day3/day3.js
@@ -0,0 +1,7 @@
+export const getDistanceToSquare1 = input => {
+  if(input === '1') {
+    return 0;
+  }
+
+  
+}
---
+++ day3/day3.test.js
@@ -0,0 +1,12 @@
+
+import { getDistanceToSquare1 } from "./day3";
+describe('Day 3', () => {
+  
+  it('should calculate correct number of steps to 1', () => {
+    /* expect(getDistanceToSquare1(1)).toEqual(0);
+    expect(getDistanceToSquare1(12)).toEqual(3);
+    expect(getDistanceToSquare1(23)).toEqual(2);
+    expect(getDistanceToSquare1(1024)).toEqual(31); */
+  });
+  
+});
